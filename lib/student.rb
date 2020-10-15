5bf4df6452a5d2486a0e221c97b51276ec673089
Getu Gebre <getu.gebre.gg@gmail.com>
2020-10-15 17:25:47 -0500

    done

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,12 @@
-class Student 
+class Student < User
+    attr_reader :knowledge
+    def initialize 
+        # super(first_name, last_name)
+        @knowledge = []
+    end
 
+    def learn (knowledge)
+        @knowledge << knowledge
+    end
+    
 end
